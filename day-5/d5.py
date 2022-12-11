3d8ebd017689b64510602f11346ed97a36372523
mewisss <michael.lewis3126@gmail.com>
2022-12-11 14:35:24 +1100

    d5 start

---
+++ day-5/d5.py
@@ -0,0 +1,18 @@
+file = open('data.txt', 'r')
+read = file.readlines()
+
+print(read)
+
+
+stacks = [
+    ["r","n","f","v","l","j","s","m"],
+    ["p","n","d","z","f","j","w","h"],
+    ["w","r","c","d","g"]
+    ["s","b","n"],
+    ["m","z","w","p","c","b","f","n"],
+    ["w","m","r","p"],
+    ["r","t","n","g","l","a","q"],
+    ["q","t","h","f","n","b","v"],
+    ["l","m","h","z","n","f"]
+
+]
